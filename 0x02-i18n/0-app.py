4a89fece36a3285b6cfb3cff92ead5acd35d39bf
Idris Adebowale <idrys01@gmail.com>
2024-01-09 10:47:25 +0000

    Setup a basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module define a flask aoplication
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """define the index page of the app
+    """
+    return render_template("0-index.html")
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,3 @@
+# Internationalozation (i18n) and Localization (l10n)
+## Introduction
+This project deals with making Applications univerally accesible with respect to language, i.e Translation from one language to the other.
